d0869f5a3311e8f7e8398b4be93e3fcaade3420c
GeraJuarez <geracjuarez@gmail.com>
2019-10-12 14:22:35 -0500

    more fixes

--- binarization.py
+++ binarization.py
@@ -3,12 +3,12 @@ import numpy as np
 from cv_helpers import plt_hist, start_cv_video, plt_show_img
 from cv2 import cv2
 
-def grayscale_binarization(img, threshold=127): 
-    _ , img = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
+def grayscale_binarization(img, threshold=127, bin_val=255): 
+    _ , img = cv2.threshold(img, threshold, bin_val, cv2.THRESH_BINARY)
     return img
 
-def otsu_binarization(img):
-    _, binarized_img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+def otsu_binarization(img, bin_val=255):
+    _, binarized_img = cv2.threshold(img, 0, bin_val, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     return binarized_img
 
 def mask_binarization(img):
@@ -17,14 +17,14 @@ def mask_binarization(img):
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        start_cv_video(img_filter=otsu_grayscale_binarization)
+        start_cv_video(img_filter=otsu_binarization)
     
     else:
         try:
             img_path = sys.argv[1]
             img = cv2.imread(img_path, cv2.IMREAD_COLOR)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            binarized_img = otsu_grayscale_binarization(img)
+            binarized_img = otsu_binarization(img)
             plt_show_img(binarized_img)
 
             if (sys.argv[2] == '-h'):
