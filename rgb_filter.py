4964bd92c7c3f9dab30c4393b96523d5618a214f
GeraJuarez <geracjuarez@gmail.com>
2019-10-13 14:04:36 -0500

    rgb filter refactor

--- rgb_filter.py
+++ rgb_filter.py
@@ -1,29 +1,45 @@
 import numpy as np
 from cv2 import cv2
 from cv_helpers import cv2_show_img, plt_show_img, start_cv_video
-from binarization import otsu_binarization
+from binarization import otsu_binarization, grayscale_binarization
 
 def rgb_filter(img, *params):
     red, green, blue = cv2.split(img)
-    binarized_r = otsu_binarization(red)
-    binarized_g = otsu_binarization(green)
-    binarized_b = otsu_binarization(blue)
+    binarized_r = grayscale_binarization(red, bin_val=1, threshold=100)
+    binarized_g = grayscale_binarization(green, bin_val=1, threshold=100)
+    binarized_b = grayscale_binarization(blue, bin_val=1, threshold=100)
 
-    #binarized_r = np.where(binarized_r==255, 0, binarized_r)
-    #binarized_g = np.where(binarized_g==255, 0, binarized_g)
-    #binarized_b = np.where(binarized_b==255, 0, binarized_b)
-    mask = cv2.merge( (binarized_r, binarized_g, binarized_b) )
+    gb_mask = np.bitwise_or(binarized_g, binarized_b)
+    red_mask = np.bitwise_xor(binarized_r, gb_mask)
+    red_mask = np.bitwise_and(red_mask, binarized_r)
+
+    rb_mask = np.bitwise_or(binarized_r, binarized_b)
+    green_mask = np.bitwise_xor(binarized_g, rb_mask)
+    green_mask = np.bitwise_and(green_mask, binarized_g)
+
+    rg_mask = np.bitwise_or(binarized_r, binarized_g)
+    blue_mask = np.bitwise_xor(binarized_b, rg_mask)
+    blue_mask = np.bitwise_and(blue_mask, binarized_b)
+
+    #red_mask = np.where(binarized_r==1, 0, binarized_r)
+    #green_mask = np.where(binarized_g==1, 0, binarized_g)
+    #blue_mask = np.where(binarized_b==1, 0, binarized_b)
+
+    rgb_mask = np.bitwise_or(red_mask, green_mask)
+    rgb_mask = np.bitwise_or(rgb_mask, blue_mask)
+    mask = cv2.merge( (rgb_mask, rgb_mask, rgb_mask) )
+    #mask = cv2.merge( (red_mask, green_mask, blue_mask) )
 
     if len(params) > 0:
-        kernel_size = int(params[0]) if len(params) > 0 else 5
+        kernel_size = int(params[0]) if len(params) > 0 else 3
         dilate_iter = int(params[1]) if len(params) > 1 else 0
         erode_iter = int(params[2]) if len(params) > 2 else 0
 
         kernel = np.ones((kernel_size,kernel_size), np.uint8)
         mask = cv2.dilate(mask, kernel, iterations=dilate_iter)
         mask = cv2.erode(mask, kernel, iterations=erode_iter) 
-
-    return mask
+   
+    return img * mask
 
 if __name__ == '__main__':
     import argparse
