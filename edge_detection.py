6b80ca6cc4df4a6e0e5c175d6587f79cd920644d
GeraJuarez <geracjuarez@gmail.com>
2019-10-31 17:18:56 -0600

    update default val in canny

--- edge_detection.py
+++ edge_detection.py
@@ -38,7 +38,7 @@ def apply_prewitt(img, *params):
 
 def apply_canny(img, *params):
     low_threshold = 100
-    high_threshold = 200
+    high_threshold = 70
     img = cv2.Canny(img, low_threshold, high_threshold)
 
     return img
