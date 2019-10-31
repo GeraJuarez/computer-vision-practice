173fb7b2d80684559f71ef85c02e9af68f99d227
GeraJuarez <geracjuarez@gmail.com>
2019-10-31 16:51:15 -0600

    contour detections

--- edge_detection.py
+++ edge_detection.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 from cv2 import cv2
 from cv_helpers import start_cv_video, plt_show_img
+from binarization import adaptative_biarization
 
 
 def apply_sobel(img, *params):
@@ -41,6 +42,39 @@ def apply_canny(img, *params):
     img = cv2.Canny(img, low_threshold, high_threshold)
 
     return img
+
+def get_contours(rgb_img, color=(255,0,255), thickness=4):
+    gray_img = cv2.cvtColor(rgb_img, cv2.COLOR_RGB2GRAY)
+    thresh = adaptative_biarization(gray_img)
+    _, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    new_img = rgb_img.copy()
+    index = -1 # All contours
+    cv2.drawContours(new_img, contours, index, color, thickness)
+
+    return new_img
+
+def calculate_area_perimeter(rgb_img, color=(255,0,255), thickness=4):
+    gray_img = cv2.cvtColor(rgb_img, cv2.COLOR_RGB2GRAY)
+    thresh = adaptative_biarization(gray_img)
+    _, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    index = -1
+    h, w, _ = rgb_img.shape
+
+    objects = np.zeroes([h, w, 1], 'uint8')
+    for c in contours:
+        cv2.drawContours(objects, [c], index, color, -1)
+        area = cv2.contourArea(c)
+        perimeter = cv2.arcLength(c, closed=True)
+
+        Moment = cv2.moments(c)
+        cx = int( Moment['m10'] / Moment['m00'] )
+        cy = int ( Moment['m01'] / Moment['m00'] )
+        cv2.circle(objects, (cx, cy), 4, (0,0,255), -1)
+        print(f'Area: {area}, Perimeter:{perimeter}')
+
+    return objects
 
 if __name__ == '__main__':
     try:
